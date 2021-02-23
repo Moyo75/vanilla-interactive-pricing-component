9938f9827f27077449b5feaac5de8839464a7d88
John Ademoye <moyojohn188@gmail.com>
2021-02-23 18:17:41 +0000

    Started the mobile view

--- app/js/script.js
+++ app/js/script.js
@@ -7,19 +7,17 @@ const pageViewAmount = domSelector('.pageview_amount');
 const yearlyBillingToggle = domSelector('.yearly_billing_toggle');
 
 let pricingArray = [8, 12, 16, 24, 36];
-const pageViews = [10, 50, 100, 500, 1000];
+const pageViews = ['10K', '50K', '100K', '500K', '1M'];
 
-console.log(pricingArray);
+function handleEvents() {
+  const { value } = rangeInput;
 
-rangeInput.addEventListener('input', function () {
-  pageViewValue.textContent = this.value;
+  pageViewValue.textContent = pageViews[value - 1];
 
-  pageViews.forEach((view, index) => {
-    if (this.value == pageViews[index]) {
-      pageViewAmount.textContent =
-        yearlyBillingToggle.checked === true
-          ? (pricingArray[index] * 0.25).toFixed(2)
-          : pricingArray[index].toFixed(2);
-    }
-  });
-});
+  pageViewAmount.textContent = yearlyBillingToggle.checked
+    ? (pricingArray[value - 1] * 0.25).toFixed(2)
+    : pricingArray[value - 1].toFixed(2);
+}
+
+yearlyBillingToggle.addEventListener('change', () => handleEvents());
+rangeInput.addEventListener('input', () => handleEvents());
